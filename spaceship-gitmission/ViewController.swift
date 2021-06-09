cbddddee2bb38819e83af125357b806004a58904
truekevinic <kepin33@gmail.com>
2021-06-09 16:49:15 +0700

    Initial Commit

---
+++ spaceship-gitmission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spaceship-gitmission
+//
+//  Created by Kevin Leon on 09/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
